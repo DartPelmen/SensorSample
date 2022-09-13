eaa025df4522a433094915fcde1ed37303c53687
Ivan Kuznetsov <kyzaiv1@yandex.ru>
2022-09-13 16:05:02 +1000

    комментарии

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,45 +13,66 @@ import java.util.function.Consumer
 
 
 class MainActivity : AppCompatActivity() {
+    /**
+     * Обработчик событий SensorEvent (получение значений с сенсора и изменения точности измерения)
+     * */
     private val listener = object : SensorEventListener{
+        /**
+         * Метод обратного вызова для обработки получения значения сенсора.
+         * Обратите внимание на обработку получаемой информации
+         * */
         override fun onSensorChanged(p0: SensorEvent?) {
             binding.xField.text = "x: ${p0?.values?.get(0)?.toString()}";
-            binding.yField.text = "y: "+ p0?.values?.get(1)?.toString();
-            binding.zField.text = "z: "+ p0?.values?.get(2)?.toString();
-
+            binding.yField.text = "y: ${p0?.values?.get(1)?.toString()}";
+            binding.zField.text = "z: ${p0?.values?.get(2)?.toString()}";
         }
-
+        /**
+         * Метод обратного вызова для обработки изменения точности измерения
+         * */
         override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
         }
     }
     private lateinit var binding: ActivityMainBinding
+    /**
+     * Описание сенсора
+     * */
     private var sensor: Sensor? = null
+    /**
+     * Менеджер сенсоров, предоставляемый системным сервисом
+     * */
     private var s: SensorManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //получение списка сенсоров
         s = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        //вывод списка сенсоров
         s?.getSensorList(Sensor.TYPE_ALL)?.forEach(Consumer { x: Sensor ->
                 Log.d(
                     "SENSOR",
                     x.name
                 )
             })
+        //получение гироскопа по умолчанию (при наличии)
             sensor = if(s?.getSensorList(Sensor.TYPE_GYROSCOPE)?.size!! >0) s?.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
                 else null
+        //регистрация прослушивателя гироскопа (при наличии)
             sensor?.let {
-                s?.registerListener(listener,it,5000)
+                s?.registerListener(listener,it,SensorManager.SENSOR_DELAY_NORMAL)
             }
         }
 
     override fun onResume() {
         super.onResume()
+        //регистрация прослушивателя гироскопа (при наличии)
         s?.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onStop() {
         super.onStop()
+        //снятие регистрации прослушивателя гироскопа (при наличии)
         s?.unregisterListener(listener)
     }
 
